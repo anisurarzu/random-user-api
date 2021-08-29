ee12ca52ef5b6b851835e126e5d5bb18c258d678
anisurarzu <62845237+anisurarzu@users.noreply.github.com>
2021-08-29 12:46:37 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const loadUser = () => {
+  fetch("https://randomuser.me/api/?results=50")
+    .then((res) => res.json())
+    .then((data) => displayUser(data.results[0]));
+};
+
+const displayUser = (user) => {
+  console.log(user);
+  const displayUser = document.getElementById("display-user");
+  displayUser.textContent = "";
+  const div = document.createElement("div");
+
+  div.innerHTML = `<img class="img" src="${user.picture.large}">
+    <p>Name: ${user.name.title} ${user.name.first} ${user.name.last}</p>
+ <p>City: ${user.location.city}</p>
+ <p>Street-Number: ${user.location.street.number}</p>
+
+ <p>Timezone: ${user.registered.date}</p>
+  `;
+  displayUser.appendChild(div);
+
+  //
+};
+
+// document.getElementById("details").addEventListener("click", (e) => {});
+
+// loading js
+window.addEventListener("load", () => {
+  const loader = document.querySelector(".loader");
+  loader.className += " hidden"; // class "loader hidden"
+});
